c6cbacd1fb4d832020bbde4b0aa985f37227d763
freewind <nowindlee@gmail.com>
2019-05-09 14:04:35 +0800

    demo

--- hello.ts
+++ hello.ts
@@ -1,1 +1,21 @@
-console.log('Hello');
+type AnyTypeNot<T, K> = T extends K ? never : T;
+
+type AnyTypeNotString<T> = AnyTypeNot<T, string>
+
+function toString<T>(value: AnyTypeNotString<T>): string {
+  return value.toString();
+}
+
+console.log(toString(3));
+console.log(toString('3'));
+
+
+type AnyTypeNotPromise<T> = AnyTypeNot<T, Promise<any>>
+
+function run<T>(fn: () => AnyTypeNotPromise<T>): T {
+  return fn();
+}
+
+console.log(run(() => 3));
+console.log(run(() => Promise.resolve(3)));
+
